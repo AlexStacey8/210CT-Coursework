11bcf38c4add69cc238407ea2a5865e1b02513cc
AlexStacey8 <alex_stacey@hotmail.co.uk>
2016-10-23 15:02:55 +0100

    Week 2 Task 1
    Python code for highest perfect square function

---
+++ HighestPerfectSquare.py
@@ -0,0 +1,18 @@
+#week 3 task 3 basic
+
+def HighestPerfectSquare(x):
+    """Given a positive integer this fuction will return the highest perfect square that is either equal or less than the number"""
+
+    n = 1 #number to test 
+    perfectSquare = 1 #current highest perfect square
+        
+    while n**2 <= x :
+        perfectSquare = n**2
+        n = n+1
+        
+        
+    return("The Highest perfect suqare is >> " + str(perfectSquare))
+
+        
+
+        
